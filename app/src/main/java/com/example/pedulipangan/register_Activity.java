4ac9fa3f316004fea7ef1882b2a5f7a9f5c2e0ba
Rucita Rena Mesiasana <your email>
2025-07-04 21:12:58 +0700

    ya

--- app/src/main/java/com/example/pedulipangan/register_Activity.java
+++ app/src/main/java/com/example/pedulipangan/register_Activity.java
@@ -3,16 +3,21 @@ package com.example.pedulipangan;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.pedulipangan.ui.home.HomeFragment;
 import com.example.pedulipangan.ui.login.login_Activity;
 
 public class register_Activity extends AppCompatActivity {
 
     ImageView btnBack;
     TextView loginLink;
+
+    Button btnRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,7 @@ public class register_Activity extends AppCompatActivity {
 
         btnBack = findViewById(R.id.btnBack);
         loginLink = findViewById(R.id.loginLink);
+        btnRegister = findViewById(R.id.btnRegister);
 
         // kembali ke onboarding saat tekan panah
         btnBack.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +38,12 @@ public class register_Activity extends AppCompatActivity {
             }
         });
 
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toLogin();
+            }
+        });
         // navigasi ke login
         loginLink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,5 +52,10 @@ public class register_Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    public void toLogin(){
+        Intent intent = new Intent(register_Activity.this, login_Activity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/pedulipangan/ui/login/login_Activity.java
+++ app/src/main/java/com/example/pedulipangan/ui/login/login_Activity.java
@@ -3,6 +3,7 @@ package com.example.pedulipangan.ui.login;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +11,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.pedulipangan.R;
 import com.example.pedulipangan.onboarding_Activity;
 import com.example.pedulipangan.register_Activity;
+import com.example.pedulipangan.ui.home.HomeFragment;
 
 public class login_Activity extends AppCompatActivity {
 
     ImageView btnBack;
     TextView createAccount;
+    Button btnLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,7 @@ public class login_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         btnBack = findViewById(R.id.btnBack);
+        btnLogin = findViewById(R.id.btnLogin);
         createAccount = findViewById(R.id.createAccount);
 
         // kembali ke onboarding saat tekan panah
@@ -34,6 +39,13 @@ public class login_Activity extends AppCompatActivity {
             }
         });
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toMain();
+            }
+        });
+
         // navigasi ke register
         createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,5 +54,11 @@ public class login_Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void toMain(){
+        Intent intent = new Intent(login_Activity.this, HomeFragment.class);
+        startActivity(intent);
+        finish();
     }
 }
